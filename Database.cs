a21a3a1a572e1f6405baaecbb0c35520374022c8
nimrodga <164754449+nimrodga@users.noreply.github.com>
2026-03-22 09:08:52 -0600

    Implement basic database operations for Person
    Este es el Class que sobre la Database SQLite sobre Tareas, y aunque utiliza como objeto principal Person, el contexto es Tarea.

--- Database.cs
+++ Database.cs
@@ -1,1 +1,40 @@
+using System;
+using SQLite;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
+namespace AgendaAndroid
+{
+    public class Database 
+    {         
+        private readonly SQLiteAsyncConnection _database;
+        
+        public Database (string dbpath)
+        {
+        	_database = new SQLiteAsyncConnection(dbpath);
+        	_database.CreateTableAsync<Person>();
+        }
+        public Task <List<Person>> GetPeopleAsync()
+        {
+        	return _database.Table<Person>().ToListAsync();
+        }
+        public Task<int> SavePersonAsync(Person person)
+        {
+        	return _database.InsertAsync(person);
+        	
+        }
+        public Task <int> DeletePersonAsync(Person person)
+		{
+			return _database.DeleteAsync(person);
+		}
+		public async Task <int> ToggleItemStatusAsync(Person person)
+		{
+			person.Subscribed = !person.Subscribed;
+			return await _database.UpdateAsync(person);
+		}
+		
+    }
+}
